6882ba71a37fd0356b928485e22124cc643a53bf
shivamgupta57121 <shivamgupta57121@gmail.com>
2021-06-15 19:05:10 +0530

    Queens Combinations - 2d As 1d - Queen Chooses

---
+++ Level2/Recursion And Backtracking/queenCombAs1dQueenChooses.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void queensCombinations(int qpsf, int tq, boolean[][] chess, int lcno) {
+        // qpsf - queen placed so far
+        // tq - total queen
+        // chess - current placement 
+        // lcno - last cell no used
+        if(qpsf == tq){
+            for(boolean arr[] : chess){
+                for(boolean vl : arr){
+                    if(vl){
+                        System.out.print("q\t");
+                    } else {
+                        System.out.print("-\t");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        for(int i = lcno+1 ; i < tq*tq ; i++){
+            int r = i / tq;
+            int c = i % tq;
+            chess[r][c] = true;
+            queensCombinations(qpsf+1, tq, chess, i);
+            chess[r][c] = false;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] chess = new boolean[n][n];
+
+        queensCombinations(0, n, chess, -1);
+    }
+}
