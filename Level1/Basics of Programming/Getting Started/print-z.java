de9e6b5e80cef028e78208c78ffc91b608e92a10
shivamgupta57121 <shivamgupta57121@gmail.com>
2022-01-23 16:07:11 +0530

    Print Z

---
+++ Level1/Basics of Programming/Getting Started/print-z.java
@@ -0,0 +1,20 @@
+// Print Z of size 5
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // Write your code here
+    // Print 5 star 
+    System.out.println("*****");
+    // Print 3 space 1 star 
+    System.out.println("   *");
+    // Print 2 space 1 star 
+    System.out.println("  *");
+    // Print 1 space 1 star 
+    System.out.println(" *");
+    // Print 5 star 
+    System.out.print("*****");
+    
+  }
+}
