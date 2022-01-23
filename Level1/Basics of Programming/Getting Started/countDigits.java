57376d6f9328bdcf2fac465d9be72dafd58f90c7
shivamgupta57121 <shivamgupta57121@gmail.com>
2022-01-23 16:41:38 +0530

    Count Digits In A Number

---
+++ Level1/Basics of Programming/Getting Started/countDigits.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int count = 0;
+        while (n > 0) {
+            count++;
+            n /= 10;
+        }
+        System.out.println(count);
+
+    }
+}
